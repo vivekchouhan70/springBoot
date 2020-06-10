40ea2ae61b4496c2c0fcf2e857794d384f3c05ff
vivek <vivek.chouhan70@gmail.com>
2020-06-10 16:23:53 +0530

    changes done

--- SpringSe/src/main/java/com/telusko/demo/ExampleClass.java
+++ SpringSe/src/main/java/com/telusko/demo/ExampleClass.java
@@ -5,5 +5,6 @@ public class ExampleClass {
 	public static void main(String[] args) {
 		
 		System.out.println("this is created for exmaple purpose");
+		System.out.println("this is created for exmaple purpose");
 	}
 }
