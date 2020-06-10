50267b1fa402697a5f2ac358a78ce7d70738b84b
vivek <vivek.chouhan70@gmail.com>
2020-06-10 15:34:25 +0530

    initial commit

---
+++ SpringSe/src/main/java/com/telusko/demo/HomeController.java
@@ -0,0 +1,23 @@
+package com.telusko.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home() {
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/login")
+	public String login() {
+		return "login.jsp";
+	}
+	
+	@RequestMapping("/logout-success")
+	public String logout() {
+		return "logout.jsp";
+	}
+}
