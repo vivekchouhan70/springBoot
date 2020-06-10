f4854682c19bcb6ebcccd93507e61da252106415
vivek <vivek.chouhan70@gmail.com>
2020-06-10 16:42:24 +0530

    first employee commit

--- SpringSe/src/main/java/com/telusko/demo/gitsecreate.java
+++ SpringSe/src/main/java/com/telusko/demo/gitsecreate.java
@@ -4,7 +4,7 @@ public class gitsecreate{
 
 public static void main(String args[]) 
 {
-	
+	System.out.println("this is first employee");
 System.out.println("git se create ka example this is done by another employee");
 
 }
